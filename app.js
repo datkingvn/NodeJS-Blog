6c0d941d00c33a2b2a02bc103e0058265d66986d
Mạnh Đạt <tranhuudats1@gmail.com>
2023-08-11 08:45:01 +0700

    Add Login - Check Login Using JWT

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo')
 
 const connectDB = require('./server/config/db')
+const session = require("express-session");
 
 const app = express();
 const PORT = 3000 || process.env.PORT;
@@ -13,6 +16,17 @@ connectDB();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(session({
+    secret: 'dat handsome', // Một chuỗi bí mật được sử dụng để ký và mã hóa session cookie
+    resave: false, // Chỉ lưu lại session nếu có sự thay đổi trong session
+    saveUninitialized: true, // Để lưu trữ cả session chưa được khởi tạo
+    store: MongoStore.create({ // Sử dụng MongoStore để lưu trữ session vào MongoDB
+        mongoUrl: process.env.MONGODB_URL
+    }),
+}));
+
 
 app.use(express.static('public')); // CSS / Images / JS
 
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,9 +2,31 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 const User = require('../models/User');
+const bcrypt = require('bcrypt'); // Thư viện được sử dụng để mã hóa và so sánh Password
+const jwt = require('jsonwebtoken') // Thư viện để tạo và xác minh JSON Web Tokens (JWT)
+
+const adminLayout = '../views/layouts/admin';
+const jwtSecret = process.env.JWT_SECRET;
 
 
-const adminLayout = '../views/layouts/admin'
+// Admin - Check Login
+const authMiddleware = (req, res, next) => {
+    const token = req.cookies.token;
+
+    if (!token) {
+        return res.status(401).json({message: 'Unauthorized'})
+    }
+
+    try {
+        const decoded = jwt.verify(token, jwtSecret) // Xác minh tinhs hợp lệ của JWT và giải mã nó
+        req.userId = decoded.userId; // Trích xuất thông tin người dùng từ JWT đã giải mã và lưu nó vào thuộc tính userId của đối tượng yêu cầu (req)
+        next();
+    } catch (e) {
+        return res.status(401).json({message: 'Unauthorized'})
+    }
+}
+
+
 // GET Admin - Login Page
 router.get('/admin', async (req, res) => {
     const locals = {
@@ -24,19 +46,73 @@ router.post('/admin', async (req, res) => {
     try {
         const {username, password} = req.body;
 
-        if (req.body.username === 'admin' && req.body.password === 'password') {
-            res.send('You are logged in')
-        } else {
-            res.send('Wrong Username or Password')
+        const user = await User.findOne({username});
+
+        if (!user) {
+            return res.status(401).json({message: 'Invalid Credentials'})
         }
 
+        const isPasswordValid = await bcrypt.compare(password, user.password);
 
+        if (!isPasswordValid) {
+            return res.status(401).json({message: 'Invalid Credentials'})
+        }
+
+        const token = jwt.sign({userId: user._id}, jwtSecret)
+        res.cookie('token', token, {httpOnly: true})
+
+        res.redirect('/dashboard')
 
     } catch (e) {
         console.log(e)
     }
 });
 
+// POST Admin - Check Login
+router.get('/dashboard', authMiddleware, async (req, res) => {
+
+    res.render('admin/dashboard');
+
+});
+
+// router.post('/admin', async (req, res) => {
+//     try {
+//         const {username, password} = req.body;
+//
+//         if (req.body.username === 'admin' && req.body.password === 'password') {
+//             res.send('You are logged in')
+//         } else {
+//             res.send('Wrong Username or Password')
+//         }
+//
+//     } catch (e) {
+//         console.log(e)
+//     }
+// });
+
+
+// POST Admin - Register
+router.post('/register', async (req, res) => {
+    try {
+        const {username, password} = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        try {
+            const user = await User.create({
+                username, password: hashedPassword
+            })
+            res.status(201).json({message: 'User Created', user})
+        } catch (e) {
+            if (e.code === 11000 ) {
+                res.status(409).json({message: 'User already in use'})
+            }
+            res.status(500).json({message: 'Internal Server Error'})
+        }
+
+    } catch (e) {
+        console.log(e)
+    }
+});
 
 
 module.exports = router;
